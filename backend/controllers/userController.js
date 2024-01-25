7c7eb23230627bdf2ce8fa93ef531b05e401c010
riaz4519 <riaz.i3216@gmail.com>
2024-01-25 21:28:32 +0600

    user added working on login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,9 +8,42 @@ import User from '../model/userModel.js'
 // @access Public
 
 export const registerUser = asyncHandler(async (req, res, next) => {
-  res.status(201).send({
-    message: 'user registered',
+  const { name, email, password } = req.body
+
+  if (!name || !email || !password) {
+    res.status(400)
+    throw new Error('Please add all fields')
+  }
+
+  // check is user exists
+  const userExists = await User.findOne({ email: email })
+
+  if (userExists) {
+    res.status(404)
+    throw new Error('User already exists')
+  }
+
+  // hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashPassword = await bcrypt.hash(password, salt)
+
+  // create user
+  const user = await User.create({
+    name: name,
+    email: email,
+    password: hashPassword,
   })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
 })
 
 // @desc login  users
